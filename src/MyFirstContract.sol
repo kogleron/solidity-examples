77633dd51b67c3a01315c4236d029708de0d788a
kogleron <kogleron@gmail.com>
2022-04-17 11:57:44 +0300

    Enum variable type

--- src/MyFirstContract.sol
+++ src/MyFirstContract.sol
@@ -7,13 +7,22 @@ pragma solidity >=0.7.0 <0.9.0;
 // Allows to get the stored value.
 // Allows to modify the stored value.
 contract MyFirstContract {
+    enum State { waiting, ready, active}
+
+    State public state;
     string public value; // will be stored on blockchain. It has the "storage" type.
+    string public constant constValue = "some const"; // can not be modified.
 
     constructor(){
         value = "myValue";
+        state = State.waiting;
     }
 
     function set(string calldata _value) public {
         value = _value;
+    }
+
+    function activate() public {
+        state = State.active;
     }
 }
