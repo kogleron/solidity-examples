abb4da40df351b065067a8bf2c65632555630d8b
kogleron <kogleron@gmail.com>
2022-04-17 11:40:02 +0300

    Replace getter with the "public" keyword.

--- src/MyFirstContract.sol
+++ src/MyFirstContract.sol
@@ -7,14 +7,10 @@ pragma solidity >=0.7.0 <0.9.0;
 // Allows to get the stored value.
 // Allows to modify the stored value.
 contract MyFirstContract {
-    string value; // will be stored on blockchain. It has the "storage" type.
+    string public value; // will be stored on blockchain. It has the "storage" type.
 
     constructor(){
         value = "myValue";
-    }
-
-    function get() public view returns (string memory) {
-        return value;
     }
 
     function set(string calldata _value) public {
