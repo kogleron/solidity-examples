e2beca70b83d6724ffe08ebf2811132da64ee594
kogleron <kogleron@gmail.com>
2022-04-17 15:02:40 +0300

    New data types: struct, map, enum

--- src/MyFirstContract.sol
+++ src/MyFirstContract.sol
@@ -7,13 +7,25 @@ pragma solidity >=0.7.0 <0.9.0;
 // Allows to get the stored value.
 // Allows to modify the stored value.
 contract MyFirstContract {
-    enum State { waiting, ready, active}
-
+    struct Person {
+        uint256 id;
+        string firstName;
+        string lastName;
+    }
+    enum State {
+        waiting,
+        ready,
+        active
+    }
+˝
+    Person[] public people;
+    mapping(uint256 => Person) public mapPeople;
+    uint256 public peopleCount = 0;
     State public state;
     string public value; // will be stored on blockchain. It has the "storage" type.
     string public constant constValue = "some const"; // can not be modified.
 
-    constructor(){
+    constructor() {
         value = "myValue";
         state = State.waiting;
     }
@@ -24,5 +36,13 @@ contract MyFirstContract {
 
     function activate() public {
         state = State.active;
+    }
+
+    function addPerson(string memory _firstName, string memory _lastName)
+        public
+    {
+        peopleCount++;
+        mapPeople[peopleCount] = Person(peopleCount, _firstName, _lastName);
+        people.push(mapPeople[peopleCount]);
     }
 }
